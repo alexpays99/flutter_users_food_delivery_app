c5c3e23051f87ae6894c812c3974a10aa26fa77e
alexpays99 <mosisa.alex@gmail.com>
2022-07-08 00:12:41 +0300

    finished UI part of ItemsDetailScreen.

--- lib/mainScreens/item_detail_screen.dart
+++ lib/mainScreens/item_detail_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:users_app/models/items.dart';
 import 'package:users_app/widgets/app_bar.dart';
+import 'package:number_inc_dec/number_inc_dec.dart';
 
 class ItemDetailScreen extends StatefulWidget {
   final Items? model;
@@ -11,10 +12,95 @@ class ItemDetailScreen extends StatefulWidget {
 }
 
 class _ItemDetailScreenState extends State<ItemDetailScreen> {
+  TextEditingController counterTextEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: MyAppBar(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.network(
+            widget.model!.thumbnailUrl.toString(),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: NumberInputPrefabbed.roundedButtons(
+              controller: counterTextEditingController,
+              incDecBgColor: Colors.amber,
+              min: 1,
+              max: 9,
+              initialValue: 1,
+              buttonArrangement: ButtonArrangement.incRightDecLeft,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              widget.model!.title.toString(),
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              widget.model!.longDescription.toString(),
+              textAlign: TextAlign.justify,
+              style: const TextStyle(
+                fontWeight: FontWeight.normal,
+                fontSize: 14,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              "€ " + widget.model!.price.toString(),
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+              ),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Center(
+            child: InkWell(
+              onTap: () {
+                //add to cart
+              },
+              child: Container(
+                decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Colors.cyan,
+                      Colors.amber,
+                    ],
+                    begin: FractionalOffset(0.0, 0.0),
+                    end: FractionalOffset(1.0, 0.0),
+                    stops: [0.0, 1.0],
+                    tileMode: TileMode.clamp,
+                  ),
+                ),
+                width: MediaQuery.of(context).size.width - 13,
+                height: 50,
+                child: const Center(
+                  child: Text(
+                    'Add to Cart',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 15,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
